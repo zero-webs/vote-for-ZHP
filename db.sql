9fbb337952a810f0720887804f63a9e6c9ff8fc5
Zero <zero.3mk.eg@gmail.com>
2026-06-11 17:02:05 +0300

    Delete db.sql

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  id TEXT PRIMARY KEY,
-  discord_id TEXT UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS votes (
-  discord_id TEXT PRIMARY KEY,
-  created_at INTEGER
-);
